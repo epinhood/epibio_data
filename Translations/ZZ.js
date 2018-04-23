8906ab4f28f9d1c16b2cdc74c06a6c7aef637256
epinhood <36595793+epinhood@users.noreply.github.com>
2018-04-23 02:08:00 -0400

    Add files via upload

---
+++ Translations/ZZ.js
@@ -0,0 +1,123 @@
+var T=new Array();
+//T["English Text"]="Translated Text"
+
+//C:\_Websites\OpenEpi2.3\OpenEpi\menu\OpenEpiMenu.htm  2009\9\7
+T["Updated "]="";
+T["updated "]="";
+T["Version"]="";
+T["Now in English, French, Spanish, and Italian"]=
+  "";
+T["Open Source Epidemiologic Statistics for Public Health"]=
+  "";
+T["accessed"]="";
+T["Suggested citation"]="";
+T["and"]="";
+T["Expand All"]="";
+T["Collapse"]="";
+T["Home"]="";
+T["Info and Help"]="";
+T["About OpenEpi"]="";
+T["News"]="";
+T["Choosing a method"]="";
+T["Using OpenEpi"]="";
+T["Credits"]="";
+T["Licensing/Disclaimer"]="";
+T["History"]="";
+T["Language/Options/Settings"]="";
+T["Calculator"]="";
+T["Counts"]="";
+T["Std.Mort.Ratio"]="";
+T["Proportion"]="";
+T["Two by Two Table"]="";
+T["Dose-Response"]="";
+T["R by C Table"]="";
+T["Matched Case Control"]="";
+T["Screening"]="";
+T["Person Time"]="";
+T["1 Rate"]="";
+T["Compare 2 Rates"]="";
+T["Continuous Variables"]="";
+T["Mean CI"]="";
+T["Median/%ile CI"]="";
+T["t test"]="";
+T["ANOVA"]="";
+T["Sample Size"]="";
+T["Unmatched CC"]="";
+T["Cohort/RCT"]="";
+T["Mean Difference"]="";
+T["Power"]="";
+T["Cohort"]="";
+T["Clinical Trial"]="";
+T["X-Sectional"]="";
+T["Random numbers"]="";
+T["Download OpenEpi"]="";
+T["Searches"]="";
+T["Google--Internet"]="";
+T["PubMed--MEDLARS"]="";
+T["Net Links"]="";
+T["Active Epi CD/Text"]="";
+T["Lots of Stat Pages"]="";
+T["Epi Info at CDC"]="";
+T["OpenEpi Prototypes"]="";
+T["Epi Supercourse"]="";
+T["Brixton Health"]="";
+T["EpiData"]="";
+T["Betty Jung Web"]="";
+T["EpiMonitor"]="";
+T["EpiInformatics"]="";
+T["Development"]="";
+T["Proposal"]="";
+T["Toolkit Description"]="";
+T["Translations"]="";
+T["JavaScript Tips"]="";
+T["<p>OpenEpi provides statistics for counts and measurements in descriptive and analytic studies, stratified analysis with exact confidence limits, matched pair and person-time analysis, sample size and power calculations, random numbers, sensitivity, specificity and other evaluation statistics, R x C tables, chi-square for dose-response, and links to other useful sites.</p>"]=
+  "";
+T["<p>OpenEpi is free and <b>open source</b> software for epidemiologic statistics. It can be run from a web server or downloaded and run without a web connection.  A server is not required. The programs are written in JavaScript and HTML, and should be compatible with recent Linux, Mac, and PC browsers, regardless of operating system. (If you are seeing this, your browser settings are allowing JavaScript.) A new tabbed interface avoids popup windows except for help files.</p>"]=
+  "";
+T["<p>Test results are provided for each module so that you can judge reliability, although it is always a good idea to check important results with software from more than one source. Links to hundreds of Internet calculators are provided.</p> "]=
+  "";
+T["<p>The programs have an open source license and can be downloaded, distributed, or translated. Some of the components from other sources have licensing statements in the source code files. Licenses referred to are available in full text at  <a href='http://OpenSource.org/licenses'>OpenSource.org/licenses</a>.  OpenEpi development was supported in part by a grant from the <a href='http://www.gatesfoundation.org'>Bill and Melinda Gates Foundation </a>to Emory University, <a href='http://www.sph.emory.edu'>Rollins School of Public Health</a>.</p>"]=
+  "";
+T["A toolkit for creating new modules and for translation is included. Please let us know if you would like to collaborate in this way. Suggestions, comments, and expressions of interest in contributing to this effort should be sent by email to: "]=
+  "";
+
+//C:\_Websites\OpenEpi2.3\OpenEpi\Etable\Settings.htm  2009\9\7
+T["Unable to save.  Please check that browser settings enable Cookies."]=
+  "";
+T["Settings saved for this browser."]="";
+T["Save Settings(for this browser)?"]="";
+T["To refresh the menu in a new language, place the cursor in the URL/Address window and press Return"]=
+  "";
+T["Save and Exit"]="";
+T["OpenEpi Settings"]="";
+T["Settings"]="";
+T["Saved"]="";
+T["Settings for 2 x 2 Input Table"]="";
+T["Disease"]="";
+T["Exposure"]="";
+T["Totals"]="";
+T["(+) on left"]="";
+T["(-) on left"]="";
+T["(+) at top"]="";
+T["(-) at top"]="";
+T["Language"]="";
+T["Select Language "]="";
+T["EN-English"]="";
+T["ES-Spanish"]="";
+T["FR-French"]="";
+T["IT-Italian"]="";
+T["PT-Portuguese"]="";
+T["Output Settings"]="";
+T["Confidence Level "]="";
+T[" Do Not highlight ´significant´ p values "]=
+  "";
+T[" Show column percents "]="";
+T[" Show row percents "]="";
+T[" Show input datatables "]="";
+T[" Show stratum results "]="";
+T["Not all applications respond to the table configuration settings. "]=
+  "";
+T["Automatic Layouts"]="";
+T[" Epi Info;Schlesselman;Lilienfeld;Friis"]="";
+T[" Kleinbaum;Breslow/Day"]="";
+T[" Custom"]="";
